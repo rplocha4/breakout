46d261d13962b2bf49d3b5ebab1db82c95996e89
Radosław Płocha <plocharadoslaw@gmail.com>
2022-07-28 17:07:37 +0200

    created funtion for detecing bricks

--- ball.py
+++ ball.py
@@ -1,5 +1,3 @@
-
-
 class Ball:
     def __init__(self, color, x, y, radius):
         self.color = color
@@ -10,18 +8,28 @@ class Ball:
         self.direcion_y = -1
         self.direcion_x = 1
 
-    def move(self, window_width, window_height):
+    def move(self, game_info):
         self.x += self.speed * self.direcion_x
         self.y += self.speed * self.direcion_y
+
+        self.detect_walls(game_info)
+
+    def detect_walls(self, game_info):
+        # hitting left wall
         if self.x <= self.radius:
             self.bounce_x()
+
+        # hitting top wall
         if self.y <= self.radius:
             self.bounce_y()
 
-        if self.x >= window_width - self.radius:
+        # hitting right wall
+        if self.x >= game_info.WIDTH - self.radius:
             self.bounce_x()
-        # if self.y >= window_height - self.radius:
-        #     self.bounce_y()
+
+        # hitting bottom wall
+        if self.y >= game_info.HEIGHT - self.radius:
+            self.stop_ball()
 
     def bounce_x(self):
         self.direcion_x *= -1
@@ -32,4 +40,19 @@ class Ball:
     def direction_left(self):
         return self.direcion_x == -1
 
+    def stop_ball(self):
+        self.speed = 0
 
+    def detect_brick(self, bricks, game_info):
+        # if int(self.x) - self.radius == int(brick.x):
+        #     if int(self.y) in range(int(brick.y), int(brick.y) + brick.height):
+        #         self.bounce_x()
+        # if int(self.x) - self.radius == int(brick.x) + brick.width:
+        #     if int(self.y) in range(int(brick.y), int(brick.y )+ brick.height):
+        #         self.bounce_x()
+
+        # hitting brick from bottom
+        for brick in bricks:
+            if int(self.y) - self.radius in range( int(brick.y) + brick.height - 5, int(brick.y) + brick.height) and \
+                    int(self.x) in range(int(brick.x), int(brick.x) + brick.width):
+                self.bounce_y()
