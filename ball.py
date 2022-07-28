07d73ca1f66db6dac0f08bc3d114089e9ea6688b
Radosław Płocha <plocharadoslaw@gmail.com>
2022-07-28 20:49:53 +0200

    moved ball hitting palette detection from palette class to ball class and removed bug which allows ball hitting wall or palette twice or more

--- ball.py
+++ ball.py
@@ -1,47 +1,54 @@
 class Ball:
-    def __init__(self, color, x, y, radius):
+    def __init__(self, color, x, y, radius, speed):
         self.color = color
         self.x = x
         self.y = y
         self.radius = radius
-        self.speed = 5
-        self.direcion_y = -1
-        self.direcion_x = 1
+        self.speed = speed
+        self.direction_y = -1
+        self.direction_x = 1
+        self.hitted_wall = False
+        self.hitted_palette = False
 
     def move(self, game_info):
-        self.x += self.speed * self.direcion_x
-        self.y += self.speed * self.direcion_y
+        self.x += self.speed * self.direction_x
+        self.y += self.speed * self.direction_y
 
         self.detect_walls(game_info)
 
     def detect_walls(self, game_info):
-        # hitting left wall
-        if self.x <= self.radius:
-            self.bounce_x()
+        if not self.hitted_wall:
+            # hitting left wall
+            if self.x <= self.radius:
+                self.bounce_x()
+                self.change_focus()
 
-        # hitting top wall
-        if self.y <= self.radius:
-            self.bounce_y()
+            # hitting top wall
+            if self.y <= self.radius:
+                self.bounce_y()
+                self.change_focus()
 
-        # hitting right wall
-        if self.x >= game_info.WIDTH - self.radius:
-            self.bounce_x()
+            # hitting right wall
+            if self.x >= game_info.WIDTH - self.radius:
+                self.bounce_x()
+                self.change_focus()
 
-        # hitting bottom wall
-        if self.y >= game_info.HEIGHT - self.radius:
-            self.stop_ball()
+            # hitting bottom wall
+            if self.y >= game_info.HEIGHT - self.radius:
+                self.stop_ball()
+                self.change_focus()
 
     def bounce_x(self):
-        self.direcion_x *= -1
+        self.direction_x *= -1
 
     def bounce_y(self):
-        self.direcion_y *= -1
+        self.direction_y *= -1
 
     def heading_left(self):
-        return self.direcion_x == -1
+        return self.direction_x == -1
 
     def heading_down(self):
-        return self.direcion_y == 1
+        return self.direction_y == 1
 
     def stop_ball(self):
         self.speed = 0
@@ -56,30 +63,68 @@ class Ball:
 
         for brick in bricks:
             # hitting brick from bottom
-            if not self.heading_down() and int(self.y) - self.radius in range(int(brick.y) + brick.height - self.speed,
+            if not self.heading_down() and int(self.y) - self.radius in range(int(brick.y) + brick.height - self.radius,
                                                                               int(brick.y) + brick.height) \
                     and int(self.x) in range(int(brick.x) - self.radius,
                                              int(brick.x) + brick.width + self.radius):
                 self.bounce_y()
+                self.remove_focus()
                 return brick
             # hitting brick from top
-            elif self.heading_down() and int(self.y) + self.radius in range(int(brick.y) - self.speed, int(brick.y)) \
+            if self.heading_down() and int(self.y) + self.radius in range(int(brick.y) - self.radius, int(brick.y)) \
                     and int(self.x) in range(int(brick.x) - self.radius,
                                              int(brick.x) + brick.width + self.radius):
                 self.bounce_y()
+                self.remove_focus()
                 return brick
 
             # hitting brick from left
-            elif not self.heading_left() and int(self.y) in range(int(brick.y) - self.radius,
+            if not self.heading_left() and int(self.y) in range(int(brick.y) - self.radius,
                                                                   int(brick.y) + brick.height + self.radius) \
-                    and int(self.x) + self.radius in range(int(brick.x) - self.speed, int(brick.x)):
+                    and int(self.x) + self.radius in range(int(brick.x) - self.radius, int(brick.x)):
                 self.bounce_x()
+                self.remove_focus()
                 return brick
 
             # hitting brick from right
-            elif self.heading_left() and int(self.y) in range(int(brick.y) - self.radius,
+            if self.heading_left() and int(self.y) in range(int(brick.y) - self.radius,
                                                               int(brick.y) + brick.height + self.radius) \
-                    and int(self.x) - self.radius in range(int(brick.x) + brick.width - self.speed,
+                    and int(self.x) - self.radius in range(int(brick.x) + brick.width - self.radius,
                                                            int(brick.x) + brick.width):
                 self.bounce_x()
+                self.remove_focus()
                 return brick
+
+    def detect_palette(self, paddle, game_info):
+        if not self.hitted_wall and self.y >= game_info.HEIGHT - paddle.height - self.radius - 5:
+
+            # ball in first half of paddle
+            if int(self.x) in range(int(paddle.x), int(paddle.x) + paddle.width // 2 + 1):
+
+                if self.heading_left():
+                    self.bounce_y()
+                else:
+                    self.bounce_y()
+                    self.bounce_x()
+                self.change_focus()
+
+            # ball in second half of paddle
+            elif int(self.x) in range(int(paddle.x) + paddle.width // 2, paddle.width + int(paddle.x)):
+                if self.heading_left():
+                    self.bounce_y()
+                    self.bounce_x()
+                else:
+                    self.bounce_y()
+
+                self.change_focus()
+
+    def change_focus(self):
+        if self.hitted_wall:
+            self.hitted_palette = False
+        if self.hitted_palette:
+            self.hitted_wall = False
+
+    def remove_focus(self):
+        self.hitted_wall = False
+        self.hitted_palette = False
+
--- palette.py
+++ palette.py
@@ -10,25 +10,25 @@ class Palette:
         if new_x >= 0 and new_x + self.width <= game_info.WIDTH:
             self.x = new_x
 
-    def detect_ball(self, ball, game_info):
-        if ball.y >= game_info.HEIGHT - self.height - ball.radius - 5:
-
-            # ball in first half of paddle
-            if int(ball.x) in range(int(self.x), int(self.x) + self.width // 2 + 1):
-
-                if ball.heading_left():
-                    ball.bounce_y()
-                else:
-                    ball.bounce_y()
-                    ball.bounce_x()
-
-            # ball in second half of paddle
-            elif int(ball.x) in range(int(self.x) + self.width // 2, self.width + int(self.x)):
-                if ball.heading_left():
-                    ball.bounce_y()
-                    ball.bounce_x()
-                else:
-                    ball.bounce_y()
+    # def detect_ball(self, ball, game_info):
+    #     if ball.y >= game_info.HEIGHT - self.height - ball.radius - 5:
+    #
+    #         # ball in first half of paddle
+    #         if int(ball.x) in range(int(self.x), int(self.x) + self.width // 2 + 1):
+    #
+    #             if ball.heading_left():
+    #                 ball.bounce_y()
+    #             else:
+    #                 ball.bounce_y()
+    #                 ball.bounce_x()
+    #
+    #         # ball in second half of paddle
+    #         elif int(ball.x) in range(int(self.x) + self.width // 2, self.width + int(self.x)):
+    #             if ball.heading_left():
+    #                 ball.bounce_y()
+    #                 ball.bounce_x()
+    #             else:
+    #                 ball.bounce_y()
 
     def calculate_new_x(self, mouse_x, game_info):
         if mouse_x < self.width / 2:
