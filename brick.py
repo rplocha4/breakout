f7525afc5fc552d007cecbf05d13231d4c3e0fd2
Radosław Płocha <plocharadoslaw@gmail.com>
2022-07-28 17:05:34 +0200

    created brick class

---
+++ brick.py
@@ -0,0 +1,8 @@
+class Brick:
+    def __init__(self, color, x, y, width, height):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
